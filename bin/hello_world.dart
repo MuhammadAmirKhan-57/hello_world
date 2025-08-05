16f7b968eeed766b26e7d91ebe6cc6fd0fffe8ba
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-05 21:53:47 +0500

    Print Name

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,3 +1,7 @@
-void main() {
+void main() { 
+  //Function which cannot return anytype of value is void 
+  // main is top level function where execution start
   print ('Hello World');
+  //A statement is command that tells a computer to do something , also end with semicolon ; .
+  //  An Expression is a value or something that can be calculated as a value. The expression can be numbers, text, or some other type.
 }
