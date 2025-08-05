c87163985aad97cffe523a796c50e46f42cdd87e
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-05 22:51:41 +0500

    Calculator

---
+++ bin/string_interpolation/calculator.dart
@@ -0,0 +1,15 @@
+void main () {
+   int a = 10;
+   int b = 2;
+
+   int sum = a+b;
+   int sub = a-b;
+   int mult = a * b;
+   double div = a / b;
+
+   print("The sum is $sum");
+   print("The subtraction is $sub");
+   print("The multiplication is $mult");
+   print("The division is $div");
+}
+
