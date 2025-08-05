967ab237aca35db938d5a559faa2b15b6c526083
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-05 22:12:56 +0500

    getFullName Function

---
+++ bin/string_interpolation/get_full_name.dart
@@ -0,0 +1,7 @@
+void main() {
+  print(getFullName('Jhon', 'Snow'));
+}
+// Here is Function with name getFullName
+String getFullName(String name, String nickName) {
+  return "Full Name is $name $nickName";
+}
