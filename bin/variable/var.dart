789bf2e741bee96d8739df42d19aab4002089108
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-07 00:42:30 +0500

    var

---
+++ bin/variable/var.dart
@@ -0,0 +1,15 @@
+//Variable with Inferred Type
+void main() {
+  var name = 'Afzal'; // Dart knows it's a String
+  var age = 25;       // Dart knows it's an int
+  var price = 99.99;  // Dart infers double
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Price: $price');
+}
+
+//var city = 'Peshawar'; city = 123; 
+////Error: A value of type 'int' can't be assigned to a variable of type 'String'.
+
+ 
