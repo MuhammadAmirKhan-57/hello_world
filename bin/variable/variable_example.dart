e8b20ec5dd222ffdf13e8ff87414d8a7afd1477c
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-07 00:36:19 +0500

    Variable Example

---
+++ bin/variable/variable_example.dart
@@ -0,0 +1,8 @@
+void main() {
+  int age = 30;
+  String name = 'Afzal';
+  double weight = 65.5;
+  bool isProgrammer = true;
+
+  print('Name: $name, Age: $age, Weight: $weight, Programmer: $isProgrammer');
+}
