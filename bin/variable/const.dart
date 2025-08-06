01d51aa324af4462a4471c199495a6865bd279c7
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-07 00:55:02 +0500

    const var

---
+++ bin/variable/const.dart
@@ -0,0 +1,14 @@
+// Compile-Time Constant
+// const variables are known and fixed at compile time. Their value must be hardcoded or constant.
+void main () {
+
+  const pi = 3.1416;
+  const appName = 'Afzal Notes App';
+
+  print('App: $appName');
+  print('Pi value: $pi');
+}
+
+// Can’t assign runtime value
+// const now = DateTime.now(); 
+// Error: not constant at compile time
