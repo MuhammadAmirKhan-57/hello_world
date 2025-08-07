e02b232fbdb50d2cb4a215604c1038260c2c723b
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-07 23:44:02 +0500

    String

--- bin/data_types/round_double.dart
+++ bin/data_types/round_double.dart
@@ -1,3 +1,5 @@
+// Round Double Value To 2 Decimal Places
+// The .toStringAsFixed(2) is used to round the double value upto 2 decimal places in dart. You can round to any decimal places by entering numbers like 2, 3, 4, etc.
 void main () {
   double price = 3554.00340;
   print(price.toStringAsFixed(2));
---
+++ bin/data_types/string.dart
@@ -0,0 +1,8 @@
+// String
+// String helps you to store text data. You can store values like I like dart, Hello World 2140 in String. You can use single or double quotes to store string in dart.
+void main () {
+  String name = "Ali";
+  String address = "Islamabad , E9 Sector House No : 311, Street No : 11, Pk";
+
+  print("Name $name and Adress : $address");
+}
