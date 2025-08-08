f757e121e5450ae2c5729231ffbb7f998f093dc1
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-08 22:22:35 +0500

    Type Conversion/String to int

--- bin/data_types/multi_line_string.dart
+++ bin/data_types/multi_line_string.dart
@@ -1,18 +1,18 @@
 // Create A Multi-Line String In Dart
 // If you want to create a multi-line String in dart, then you can use triple quotes with either single or double quotation marks.
 
-void main () {
+void main() {
   String multiLineText = '''
   This is Multi Line Text 
   with 3 single quote
 ''';
 
-print(multiLineText);
+  print(multiLineText);
 
-String othermultiText = """
+  String othermultiText = """
 This is Multi Line Text 
 with 3 double quote
 """;
- 
- print(othermultiText);
+
+  print(othermultiText);
 }
---
+++ bin/type_conversion/string_to_int.dart
@@ -0,0 +1,16 @@
+// Convert String To Int In Dart
+// You can convert String to int using int.parse() method. The method takes String as an argument and converts it into an integer.
+
+void main () {
+  String strValue = "1";
+
+  print("Type of String value is ${strValue.runtimeType}");
+
+  int intValue = int.parse(strValue);
+
+  print("Type of Int value is $intValue ");
+
+
+  //Type of value 
+  print("Type of value is ${intValue.runtimeType}");
+}
