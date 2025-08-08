eeff9f00fca694151c0cb9176cbb4795939a7a8f
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-08 22:39:40 +0500

    int_to_string

---
+++ bin/type_conversion/int_to_string.dart
@@ -0,0 +1,14 @@
+// Convert Int To String In Dart
+// You can convert int to String using the toString() method.
+
+void mani () {
+  int numb = 1;
+  print("Type of No is $numb");
+
+  String numToStr = numb.toString();
+  print("Value of String is $numToStr");
+
+  print("Type of noToStr is ${numToStr.runtimeType}");
+
+
+}
