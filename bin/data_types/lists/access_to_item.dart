ef153ea4da1ec6d3975d973961b2536630e51725
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-11 00:17:52 +0500

    Access to item List

---
+++ bin/data_types/lists/access_to_item.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> colors = ["Red", "Green", "Blue"];
+
+  print(colors[0]);        // Red
+  print(colors.first);     // Red
+  print(colors.last);      // Blue
+  print(colors.length);    // 3
+}
