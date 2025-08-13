bcb2d85925d8e94564ebe03c0286bb1825ade39d
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-13 23:30:15 +0500

    Multipleline Comment

---
+++ bin/comments/multiple_line.dart
@@ -0,0 +1,10 @@
+// Multi-Line Comment In Dart
+// Multi-line comments start with 
+// /* and end with */ . You can write your comment inside /* and */.
+
+ void main(){  
+/*
+This is a multi-line comment.
+*/
+    print("Welcome to Technology Channel.");  
+}  
