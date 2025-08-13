df0707f2cf643a9de56a803f7f0c729432ead2ff
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-13 23:34:26 +0500

    Documentation Comment

---
+++ bin/comments/documentation_comment.dart
@@ -0,0 +1,8 @@
+// Documentation Comment In Dart
+// Documentation comments are helpful when you are writing documentation for your code. 
+// Documentation comments start with /// in dart.
+
+void main(){  
+/// This is documentation comment
+    print("Welcome to Technology Channel.");  
+}  
