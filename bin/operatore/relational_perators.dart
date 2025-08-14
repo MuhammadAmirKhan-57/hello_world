33c7209e3d8d36bc3509a9d72ddede98764d046b
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-14 23:31:29 +0500

    Relational Operators

---
+++ bin/operatore/relational_perators.dart
@@ -0,0 +1,14 @@
+// Relational Operators
+// Relational operators are also called comparison operators. They are used to make a comparison.
+
+void main() {
+  
+ int num1=10;
+ int num2=5;
+ //printing info
+ print(num1==num2); 
+ print(num1<num2);
+ print(num1>num2);
+ print(num1<=num2);
+ print(num1>=num2);
+}
