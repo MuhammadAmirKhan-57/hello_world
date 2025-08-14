48d509c7a7ad9e5fb2b0ddb5feac703a1c526881
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-14 23:21:21 +0500

    Operators Arthmetic

---
+++ bin/comments/single_line.dart
@@ -0,0 +1,8 @@
+// Single-Line Comment In Dart
+// Single line comments start with // in dart. 
+//You can write // and your text.
+
+void main() {
+// This is single-line comment.
+print("Welcome to Technology Channel.");
+}
