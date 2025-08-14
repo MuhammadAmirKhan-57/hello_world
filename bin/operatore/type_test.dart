db42f88e0bdda65a42c70efc368064f758125484
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-14 23:35:33 +0500

    Type Test Operators

---
+++ bin/operatore/type_test.dart
@@ -0,0 +1,10 @@
+// Type Test Operators
+// In Dart, type test operators are useful for checking types at runtime.
+
+void main() {
+  String value1 = "Dart Tutorial";
+  int age = 10;
+  
+  print(value1 is String);
+  print(age is !int);
+}
