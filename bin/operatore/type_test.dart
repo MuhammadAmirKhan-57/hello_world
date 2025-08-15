1032d7ab58aa61cb1e3cdd6afd2295a744d227ff
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-15 23:00:23 +0500

    Integer User Input

--- bin/operatore/type_test.dart
+++ bin/operatore/type_test.dart
@@ -1,6 +1,8 @@
 // Type Test Operators
 // In Dart, type test operators are useful for checking types at runtime.
 
+// ignore_for_file: unnecessary_type_check
+
 void main() {
   String value1 = "Dart Tutorial";
   int age = 10;
