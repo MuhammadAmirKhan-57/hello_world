5b6e9b40c7d6cd7438c1fd6734051124b804ffc3
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-16 23:27:02 +0500

    String Concatenation

---
+++ bin/data_types/string/string_concatenation.dart
@@ -0,0 +1,12 @@
+// String Concatenation
+// You can combine one String with another string. This is called concatenation. 
+// In Dart, you can use the + operator or use interpolation to concatenate the String.
+//  Interpolation makes it easy to read and understand the code.
+
+void main() {   
+String firstName = "John";
+String lastName = "Doe";
+print("Using +, Full Name is $firstName $lastName.");
+print("Using interpolation, full name is $firstName $lastName.");  
+  
+}
