a71693cf174c45c48abad8192df4d16beac31fe5
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-17 23:58:15 +0500

    full_name

---
+++ bin/basic_question_answer/full_name.dart
@@ -0,0 +1,11 @@
+// Write a program to print full name of a from first name and last name using user input.
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter first name: ");
+  String firstName = stdin.readLineSync()!;
+  stdout.write("Enter last name: ");
+  String lastName = stdin.readLineSync()!;
+  print("Full Name: $firstName $lastName");
+}
