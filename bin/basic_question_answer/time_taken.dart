5888a436590d5e33d6e629221b8b10d05b1925bb
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 00:10:21 +0500

    Time taken to reach office

---
+++ bin/basic_question_answer/time_taken.dart
@@ -0,0 +1,11 @@
+// Suppose, your distance to office from home is 25 km and you travel 40 km per hour. 
+//Write a program to calculate time taken to reach office in minutes. 
+//Formula= (distance) / (speed).
+
+void main() {
+  double distance = 25; // km
+  double speed = 40;    // km/h
+  double timeInHours = distance / speed;
+  double timeInMinutes = timeInHours * 60;
+  print("Time to reach office: $timeInMinutes minutes");
+}
