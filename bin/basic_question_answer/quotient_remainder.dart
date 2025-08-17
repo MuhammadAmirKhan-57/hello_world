83c3ad2716a162989b9896959de284382ed5261b
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 00:00:00 +0500

    quotient and remainder

---
+++ bin/basic_question_answer/quotient_remainder.dart
@@ -0,0 +1,9 @@
+// Write a program to find quotient and remainder of two integers.
+
+void main() {
+  int a = 20, b = 3;
+  int quotient = a ~/ b;
+  int remainder = a % b;
+  print("Quotient: $quotient");
+  print("Remainder: $remainder");
+}
