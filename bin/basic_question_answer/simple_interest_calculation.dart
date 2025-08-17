3698cf25c327338d91afe39d76f30ed8217c9eb4
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-17 23:53:04 +0500

    ormula= (p * t * r) / 100

---
+++ bin/basic_question_answer/simple_interest_calculation.dart
@@ -0,0 +1,10 @@
+// Write a program in Dart that finds simple interest. 
+//Formula= (p * t * r) / 100.
+
+void main() {
+  double p = 1000; // Principal
+  double t = 2;    // Time in years
+  double r = 5;    // Rate of interest
+  double si = (p * t * r) / 100;
+  print("Simple Interest = $si");
+}
