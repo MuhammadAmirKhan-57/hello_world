2a7b76304921588833f4a1a4b8f13132eabf5221
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 00:02:04 +0500

    swap two numbers

---
+++ bin/basic_question_answer/swap_two_numbers.dart
@@ -0,0 +1,11 @@
+// Write a program to swap two numbers.
+void main() {
+  int a = 10, b = 20;
+  print("Before Swap: a=$a, b=$b");
+
+  int temp = a;
+  a = b;
+  b = temp;
+
+  print("After Swap: a=$a, b=$b");
+}
