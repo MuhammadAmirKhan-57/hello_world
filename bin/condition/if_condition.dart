035f70f12455b94fa8463e349efb449cccad8b97
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 23:07:05 +0500

    Condition, If-Condition

---
+++ bin/condition/if_condition.dart
@@ -0,0 +1,31 @@
+// Conditions In Dart
+// When you write a computer program, you need to be able to tell the computer,
+// what to do in different situations. With conditions, 
+//you can control the flow of the dart program. 
+//Suppose you need to execute a specific code when a particular situation is true. 
+//In that case, you can use conditions in Dart. 
+//E.g., a calculator app must perform subtraction if the user presses the subtract button,
+// and addition if the user taps the add button.
+
+// Types Of Condition
+// You can use following conditions to control the flow of your program.
+
+// If Condition
+// If-Else Condition
+// If-Else-If Condition
+// Switch case
+
+
+// If Condition
+// The easy and most common way of controlling the flow of a program is through the use of an if statement. 
+//If statement allow us to execute a code block when the given condition is true. 
+//Conditions evaluate boolean values.
+
+void main()
+{
+    var age = 20;
+    
+    if(age >= 18){
+      print("You are voter.");
+    }
+}
