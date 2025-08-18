5276df93af160fdca3814425cad6d126c084d876
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 23:08:43 +0500

    If_else_condition

---
+++ bin/condition/if_else_condition.dart
@@ -0,0 +1,12 @@
+// If-Else Condition
+// If the result of the condition is true, then the body of the if-condition is executed. 
+//Otherwise, the body of the else-condition is executed.
+
+void main(){
+        int age = 12;
+        if(age >= 18){
+            print("You are voter.");
+        }else{
+            print("You are not voter.");
+        }
+}
