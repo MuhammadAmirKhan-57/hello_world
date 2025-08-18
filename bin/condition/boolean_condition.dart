c3682866bbd1928bc7e61d3415861f3fe5803dc9
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-18 23:22:02 +0500

    Greatest_number

---
+++ bin/condition/boolean_condition.dart
@@ -0,0 +1,9 @@
+// Condition Based On Boolean Value
+// If the married status is false, it prints you are single;
+//otherwise, it will print you are married.
+
+void main() {
+  bool isMarried = false;
+  print(isMarried ? "You are married." : "You are single.");
+}
+
---
+++ bin/condition/greatest_number.dart
@@ -0,0 +1,18 @@
+// Find Greatest Number Among 3 Numbers
+// Dart program, which finds the greatest number among three numbers.
+
+void main(){
+        int num1 = 1200;
+        int num2 = 1000;
+        int num3 = 150;
+
+        if(num1 > num2  && num1 > num3){
+            print("Num 1 is greater: i.e $num1");
+        }
+        if(num2 > num1 && num2 > num3){
+           print("Num2 is greater: i.e $num2");
+        }
+        if(num3 > num1 && num3 > num2){
+            print("Num3 is greater: i.e $num3");
+        }
+}
