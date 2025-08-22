6745fe6e72ceb6440f7070a2200fb589989d8a24
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-22 23:58:20 +0500

    Do While Loop

---
+++ bin/condition/loops/do while/do_while.dart
@@ -0,0 +1,17 @@
+// Do While Loop
+// Do while loop is used to run a block of code multiple times. 
+// The loop’s body will be executed first, and then the condition is tested.
+
+// First, it runs statements, and finally, the condition is checked.
+// If the condition is true, the code inside {} is executed.
+// The condition is re-checked until the condition is false.
+// When the condition is false, the loop stops.
+
+void main() {
+  int i = 1;
+  do {
+    print(i);
+    i++;
+  } while (i <= 10);
+}
+
