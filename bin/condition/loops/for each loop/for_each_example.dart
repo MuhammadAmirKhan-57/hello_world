75bf61f86d7a5d1655dc1cba7a1c5892435c3826
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-23 22:56:52 +0500

    Exception Handling

---
+++ bin/condition/loops/for each loop/for_each_example.dart
@@ -0,0 +1,8 @@
+// For Each Loop
+// The for each loop iterates over all list elements or variables.
+// It is useful when you want to loop through list/collection.
+
+void main() {
+  List<String> footballplayers = ['Ronaldo', 'Messi', 'Neymar', 'Hazard'];
+  footballplayers.forEach((names) => print(names));
+}
