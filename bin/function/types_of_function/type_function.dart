e07db3a9bcee8d129d81a19463aa01c882baf2b1
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-26 22:51:24 +0500

    Function No Prameter NoReturn Type

---
+++ bin/function/types_of_function/type_function.dart
@@ -0,0 +1,19 @@
+// Types Of Function
+// Functions are the block of code that performs a specific task. Here are different types of functions:
+
+// No Parameter And No Return Type
+// Parameter And No Return Type
+// No Parameter And Return Type
+// Parameter And Return Type
+
+// Function With No Parameter And No Return Type
+// In this function, you do not pass any parameter and expect no return type.
+
+void main() {
+  print("Function With No Parameter and No Return Type");
+  printPrimeMinisterName();
+}
+
+void printPrimeMinisterName() {
+  print("John Doe.");
+}
