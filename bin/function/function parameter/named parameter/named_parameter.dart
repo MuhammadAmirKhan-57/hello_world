8fd4d52746ad7841b8210d795cbcc3fe08250c55
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-27 23:19:59 +0500

    Named Parameter

---
+++ bin/function/function parameter/named parameter/named_parameter.dart
@@ -0,0 +1,17 @@
+// Named Parameter In Dart
+// Dart allows you to use named parameters to clarify the parameter’s meaning in function calls. 
+// Curly braces {} are used to specify named parameters.
+
+void printInfo({String? name, String? gender}) {
+  print("Hello $name your gender is $gender.");
+}
+
+void main() {
+  // you can pass values in any order in named parameters.
+  printInfo(gender: "Male", name: "John");
+  printInfo(name: "Sita", gender: "Female");
+  printInfo(name: "Reecha", gender: "Female");
+  printInfo(name: "Reecha", gender: "Female");
+  printInfo(name: "Harry", gender: "Male");
+  printInfo(gender: "Male", name: "Santa");
+}
