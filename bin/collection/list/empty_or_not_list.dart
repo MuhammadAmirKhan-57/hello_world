f2aca0ba4b730b30f9dfb00ad232fe424f291393
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-31 23:54:40 +0500

    List Empty Or List

---
+++ bin/collection/list/empty_or_not_list.dart
@@ -0,0 +1,9 @@
+void main() {
+   List<String> drinks = ["water", "juice", "milk", "coke"];
+   List<int>  ages = [];
+   print("Is drinks Empty: ${drinks.isEmpty}");
+   print("Is drinks not Empty: ${drinks.isNotEmpty}");
+   print("Is ages Empty: ${ages.isEmpty}");
+   print("Is ages not Empty: ${ages.isNotEmpty}");
+   
+}  
