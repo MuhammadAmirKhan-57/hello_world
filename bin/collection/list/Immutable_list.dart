0cb8184459d6eadfe9747181880cac8a7ea92321
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-31 23:51:17 +0500

    Immutable List

--- bin/collection/list/Immutable_list.dart
+++ bin/collection/list/Immutable_list.dart
@@ -1,6 +1,6 @@
 void main() {
   const List<String> colors = ['Red', 'Green', 'Blue'];
 
-  // colors.add('Yellow'); // ❌ Error: Cannot modify an unmodifiable list
+  // colors.add('Yellow'); // Error: Cannot modify an unmodifiable list
   print(colors); // Output: [Red, Green, Blue]
 }
