6fd4f580dec0d278788e2b800b1708de95d67c3b
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-09-04 23:15:12 +0500

    name list

---
+++ bin/collection/map/map.dart
@@ -0,0 +1,14 @@
+// Map In Dart :
+// In a Map, data is stored as keys and values. 
+// In Map, each key must be unique. 
+// They are similar to HashMaps and Dictionaries in other languages.
+
+
+void main(){
+Map<String, String> countryCapital = {
+  'USA': 'Washington, D.C.',
+  'India': 'New Delhi',
+  'China': 'Beijing'
+};
+  print(countryCapital);
+}
---
+++ bin/condition/question_answer/name_list.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> names = ["Ali", "Bilal", "Saad", "Omar", "Hassan"];
+  
+  print("Names in the list:");
+  for (var name in names) {
+    print(name);
+  }
+}
