10a5cccdc99e3629d4cc00222168a9e204bbf59d
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-09-14 22:44:07 +0500

    Class Person

---
+++ bin/oop/class/person_class.dart
@@ -0,0 +1,13 @@
+class Person {
+  String? name;
+  String? phone;
+  bool? isMarried;
+  int? age;
+
+  void displayInfo() {
+    print("Person name: $name.");
+    print("Phone number: $phone.");
+    print("Married: $isMarried.");
+    print("Age: $age.");
+  }
+}
