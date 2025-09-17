5e8e14ef6807aa7167d253532cf8a61d927331ee
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-09-17 21:59:18 +0500

    Parameterized Constructor

---
+++ bin/oop/constructor/parameterized constructor/parameterized_constructor.dart
@@ -0,0 +1,20 @@
+// Parameterized Constructor
+// Parameterized constructor is used to initialize the instance variables of the class.
+//  Parameterized constructor is the constructor that takes parameters. 
+// It is used to pass the values to the constructor at the time of object creation.
+
+class Student {
+  String? name;
+  int? age;
+  int? rollNumber;
+  // Constructor
+  Student(this.name, this.age, this.rollNumber);
+}
+
+void main(){
+    // Here student is object of class Student. 
+    Student student = Student("John", 20, 1);
+    print("Name: ${student.name}");
+    print("Age: ${student.age}");
+    print("Roll Number: ${student.rollNumber}");
+}
